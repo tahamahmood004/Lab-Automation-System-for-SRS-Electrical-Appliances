081c60bb20754023466f71a79236ad99c6cacc9b
Taha Mahmood <115484725+tahamahmood004@users.noreply.github.com>
2024-02-04 14:21:21 +0500

    Delete pages directory

--- pages/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db_server = "localhost";
-    $db_user = "root";
-    $db_password = "";
-    $db_name = "electroverifypro";
-    $conn = "";
-
-    $conn = mysqli_connect($db_server, $db_user , $db_password, $db_name);
